7d4a4bd9fd27b0d279b200b8705aacb09d99952d
Yasser Bazzi <32601969+YShow@users.noreply.github.com>
2020-11-10 07:55:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp zramswapdisable /usr/local/sbin/
 cp zramswapenable /usr/local/sbin/
+chown root:root /usr/local/sbin/zramswapenable
+chown root:root /usr/local/sbin/zramswapdisable
 
 cp systemd-zramswap.service /etc/systemd/system/
 
