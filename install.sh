5f2895cd54d703964ec99abf957779da6b691637
Yasser Bazzi <32601969+YShow@users.noreply.github.com>
2020-11-10 08:06:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cp zramswapdisable /usr/local/sbin/
 cp zramswapenable /usr/local/sbin/
-chown root:root /usr/local/sbin/zramswapenable
-chown root:root /usr/local/sbin/zramswapdisable
-
 cp systemd-zramswap.service /etc/systemd/system/
+
+chown root:root /usr/local/sbin/zramswapdisable
+chown root:root /usr/local/sbin/zramswapenable
+chown root:root /etc/systemd/system/systemd-zramswap.service
+
+chmod 0755 /usr/local/sbin/zramswapdisable
+chmod 0755 /usr/local/sbin/zramswapenable
+chmod 0644 /etc/systemd/system/systemd-zramswap.service
 
 systemctl daemon-reload
 systemctl start systemd-zramswap.service
